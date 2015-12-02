2d8c7b202fcaae4d944d2be14153fc22b0ad24cc
zhiweitai <weiertzw@163.com>
2015-12-02 17:11:13 +0800

    add templates directory and settings add template

--- polls/admin.py
+++ polls/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
 from polls.models import Question, Choice
+
 # Register your models here.
-admin.site.register(Question)
+
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
